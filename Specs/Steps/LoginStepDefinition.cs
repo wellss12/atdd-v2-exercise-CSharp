3563a49d5024126458fbf82c3a399f269fc76b9e
Wells <ll0918900@gmail.com>
2024-03-05 21:42:47 +0800

    refactor: move login logic to LoginStep class

---
+++ Specs/Steps/LoginStepDefinition.cs
@@ -0,0 +1,58 @@
+﻿using ATDD.V2.Exercise.CSharp.Specs.PageObjects;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ATDD.V2.Exercise.CSharp.Specs.Steps;
+
+[Binding]
+public sealed class LoginStepDefinition
+{
+    private readonly Browser _browser;
+    private readonly MyDbContext _dbContext;
+    private readonly LoginPage _loginPage;
+
+    public LoginStepDefinition(MyDbContext dbContext, LoginPage loginPage, Browser browser)
+    {
+        _dbContext = dbContext;
+        _loginPage = loginPage;
+        _browser = browser;
+    }
+
+    [Given(@"存在用户名为""(.*)""和密码为""(.*)""的用户")]
+    public async Task Given存在用户名为和密码为的用户(string userName, string password)
+    {
+        var allUsers = await _dbContext.Users.ToListAsync();
+        if (allUsers.Any())
+        {
+            _dbContext.Users.RemoveRange(allUsers);
+        }
+
+        await _dbContext.Users.AddAsync(new User { UserName = userName, Password = password });
+        await _dbContext.SaveChangesAsync();
+    }
+    
+    [When(@"以用户名为""(.*)""和密码为""(.*)""登录时")]
+    public void When以用户名为和密码为登录时(string userName, string password)
+    {
+        _loginPage.Open();
+        _loginPage.Login(userName, password);
+    }
+
+    [Then(@"""(.*)""登录成功")]
+    public void Then登录成功(string userName)
+    {
+        _browser.ShouldHaveText($"Welcome {userName}");
+    }
+
+    [Then(@"登录失败的错误信息是""(.*)""")]
+    public void Then登录失败的错误信息是(string errorMessage)
+    {
+        var webDriverWait = new WebDriverWait(_browser.GetWebDriver(), TimeSpan.FromSeconds(10));
+        webDriverWait
+            .Until(driver => driver.FindElement(By.XPath($"//*[text()='{errorMessage}']")))
+            .Should()
+            .NotBeNull();
+    }
+}
--- Specs/Steps/TestStepDefinitions.cs
+++ Specs/Steps/TestStepDefinitions.cs
@@ -1,11 +1,8 @@
 using System.Text;
 using System.Text.Json;
-using ATDD.V2.Exercise.CSharp.Specs.PageObjects;
 using FluentAssertions;
-using Microsoft.EntityFrameworkCore;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
-using OpenQA.Selenium.Support.UI;
 
 namespace ATDD.V2.Exercise.CSharp.Specs.Steps;
 
@@ -13,28 +10,11 @@ namespace ATDD.V2.Exercise.CSharp.Specs.Steps;
 public class TestStepDefinitions
 {
     private readonly Browser _browser;
-    private readonly MyDbContext _dbContext;
-    private readonly LoginPage _loginPage;
     private HttpResponseMessage _response;
 
-    public TestStepDefinitions(MyDbContext dbContext, Browser browser, LoginPage loginPage)
+    public TestStepDefinitions(Browser browser)
     {
-        _dbContext = dbContext;
         _browser = browser;
-        _loginPage = loginPage;
-    }
-
-    [Given(@"存在用户名为""(.*)""和密码为""(.*)""的用户")]
-    public async Task Given存在用户名为和密码为的用户(string userName, string password)
-    {
-        var allUsers = await _dbContext.Users.ToListAsync();
-        if (allUsers.Any())
-        {
-            _dbContext.Users.RemoveRange(allUsers);
-        }
-
-        await _dbContext.Users.AddAsync(new User { UserName = userName, Password = password });
-        await _dbContext.SaveChangesAsync();
     }
 
     [When(@"通过API以用户名为""(.*)""和密码为""(.*)""登录时")]
@@ -96,28 +76,5 @@ public class TestStepDefinitions
     private RemoteWebDriver GetWebDriver()
     {
         return _browser.GetWebDriver();
-    }
-
-    [When(@"以用户名为""(.*)""和密码为""(.*)""登录时")]
-    public void When以用户名为和密码为登录时(string userName, string password)
-    {
-        _loginPage.Open();
-        _loginPage.Login(userName, password);
-    }
-
-    [Then(@"""(.*)""登录成功")]
-    public void Then登录成功(string userName)
-    {
-        _browser.ShouldHaveText($"Welcome {userName}");
-    }
-
-    [Then(@"登录失败的错误信息是""(.*)""")]
-    public void Then登录失败的错误信息是(string errorMessage)
-    {
-        var webDriverWait = new WebDriverWait(_browser.GetWebDriver(), TimeSpan.FromSeconds(10));
-        webDriverWait
-            .Until(driver => driver.FindElement(By.XPath($"//*[text()='{errorMessage}']")))
-            .Should()
-            .NotBeNull();
     }
 }
